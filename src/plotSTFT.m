c555755c364d8b0fec25ce4de86e6e502cf698ff
madao0405 <hyang0405@outlook.com>
2025-03-15 23:17:30 -0700

    Delete src directory

--- src/plotSTFT.m
+++
@@ -1,9 +0,0 @@
-% File: plotSTFT.m
-function plotSTFT(signal, fs, N)
-    M = round(N/3);
-    noverlap = N - M;
-    figure;
-    spectrogram(signal, hamming(N), noverlap, N, fs, 'yaxis');
-    title(['Spectrogram (N=' num2str(N) ')']);
-    colormap jet; colorbar;
-end
